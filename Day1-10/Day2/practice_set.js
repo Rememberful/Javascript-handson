5cc0cc3d700793ab29509da0ff079e469b1102bf
Aditya Kumar <adii.utsav@gmail.com>
2025-10-15 09:47:13 +0530

    added next day

---
+++ Day1-10/Day2/practice_set.js
@@ -0,0 +1,45 @@
+// //Q1
+
+// let a = "Adi";
+// let b = 5;
+// console.log(a+b);
+
+// //Q2
+
+// console.log(typeof(a));
+// console.log(typeof(b));
+// console.log(typeof(a+b));
+
+//Q3
+
+// const adi = {
+//     "a":"aditya",
+//     "b":"bihar"
+// };
+// console.log(adi);
+// adi = "hey";
+// console.log(adi);]
+
+//Q4
+
+// const a1 = {
+//     name: "adi",
+//     class: 2
+// };
+
+// console.log(a1);
+// a1['name'] = "Yes"; //chaing the key value pair
+// a1['boola'] = true;
+// a1['class'] = {};
+// console.log(a1);
+
+// Q5
+
+const dict ={
+    "a": "Aditya",
+    "b": "bravo"
+}
+console.log(dict);
+console.log(dict.a);
+console.log(dict['a']);
+
---
+++ Day1-10/Day2/script.js
@@ -0,0 +1,62 @@
+// Arithemtic operators
+
+// let a = 45;
+// let b = 4;
+
+// console.log("a+b ", a+b);
+// console.log("a-b ", a-b);
+// console.log("a*b ", a*b);
+// console.log("a/b ", a/b);
+// console.log("a**b ", a**b);
+// console.log("a%b ", a%b);
+
+// let a = 45;
+// let b = 4;
+// console.log(a); //print original
+// console.log(a++); //first print then increase
+// console.log(a); //print a (which is now having incremented value)
+
+
+// let a = 45;
+// let b = 4;
+// console.log(a); //print original a 
+// console.log(++a); //increase by 1 then print
+// console.log(a); //print the value in a (which is incremented in last)
+
+
+// let b = 4;
+// console.log(b); //print the original
+// console.log(b--); //print b then decrement
+// console.log(b); //print the value in b(which is decremented )
+
+// let b = 4;
+// console.log(b); //print the original
+// console.log(--b); //print b then decrement
+// console.log(b); //print the value in b(which is decremented )
+
+//Assignment operator
+
+// let a = 1;
+// a += 5;
+// console.log(a);
+
+//Comparison operator 
+
+// let a = 1;
+// let b = 2;
+// console.log(a==b); //false as 1 is not equal to 2
+// console.log(a!=b); //true
+
+
+// let a = 1;
+// let b = "1";
+// console.log(a===b); 
+// console.log(a!==b);
+
+let a = 2;
+let b = 7;
+console.log(a>b && a==2);
+console.log(a<b && a==2);
+console.log(a<b && a!=2);
+
+
