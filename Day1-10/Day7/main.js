cd69f33d733fbcf80bdfa7b33a2858a40f9c58f3
Aditya Kumar <adii.utsav@gmail.com>
2025-10-18 22:41:23 +0530

    added next day

---
+++ Day1-10/Day7/main.js
@@ -0,0 +1,33 @@
+//write a program to print the marks of a student in an object using for loop
+
+// let obj = {
+//     harry: 88,
+//     rohan: 70,
+//     aakash: 90
+// };
+
+// for (const key in obj) {
+//     console.log("Marks of", key, "is", obj[key]);
+// }
+
+//Write a program to print "try again" until the user enters the correct number
+
+// let our_number = 9;
+// let your_number;
+
+// while (our_number!=your_number) {
+//     your_number = prompt("Enter a number:");
+//     if(our_number!=your_number) {
+//         console.log("Try again");
+//     }
+// }
+
+// console.log("You entered the correct number!");
+
+//Function to find mean of 5 numbers
+// function mean(a,b,c,d,e){
+//     let m = (a+b+c+d+e)/5;
+//     return m;
+// }
+
+// console.log(mean(5,6,7,8,9));
