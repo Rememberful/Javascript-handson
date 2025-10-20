8da214a7a0ce6e8bd06244b07d1d9019e420c611
Aditya Kumar <adii.utsav@gmail.com>
2025-10-20 15:51:12 +0530

    added next day

--- Day1-10/Day8/main.js
+++ Day1-10/Day8/main.js
@@ -1,4 +1,22 @@
-let name1 = "Aditya";
-for (const key of name1) {  
-    console.log(key);
-}
+// let name1 = "Aditya";
+// for (const key of name1) {  
+//     console.log(key);
+// }
+
+// console.log("Harry\"");
+// console.log("Harry\"".length);
+
+// let str1 = "Aditya is a boy";
+// console.log(str1.includes("is"));
+// console.log(str1.includes("iss"));
+// console.log(str1.startsWith("Adi"));
+// console.log(str1.startsWith("Adii"));
+// console.log(str1.endsWith("boy"));
+// console.log(str1.endsWith("boyy"));
+
+// let str = "Please give Rs 1000";
+// console.log(str.slice(15,19));
+
+// let str = "aditya";
+// console.log(str.replace("t","k"));
+
