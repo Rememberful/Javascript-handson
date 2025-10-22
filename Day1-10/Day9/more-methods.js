8cc69a8d8c193238ec6fb031121ba3580fbf3eac
Aditya Kumar <adii.utsav@gmail.com>
2025-10-22 08:14:01 +0530

    added next day

---
+++ Day1-10/Day9/more-methods.js
@@ -0,0 +1,56 @@
+// let arr = [10,4,5,6];
+// console.log(arr);
+// console.log(arr.length)
+// delete arr[1];
+// delete arr[3];
+// console.log(arr);
+// console.log(arr.length);
+
+// let a1 = [1,2,3,4];
+// let a2 = [88,9,9,0,22];
+// console.log(a1.concat(a2));
+
+// let a1 = [1,2,3,4];
+// let a2 = [88,9,9,0,22];
+// let a3 = [3,4];
+// a3.concat(a1,a2);
+// console.log(a3);
+
+// let arr = [88,44,33];
+// console.log(arr.sort());
+
+// let arr = ["a", "b", "d", "c"];
+// console.log(arr.sort());
+// let arr2 = ["Uditya", "Aditya"];
+// console.log(arr2.sort());
+
+
+// const compare = (a, b) => {
+//   return b - a;
+// };
+
+// let arr = [1, 34, 5, 6, 22];
+// arr.sort(compare);
+// console.log(arr);
+
+// let arr = [2,445,6,7];
+// console.log(arr.reverse());
+
+// let
+//  fruits = ["apple", "banana", "cherry", "date"];
+
+// let removed = fruits.splice(1, 2, "kiwi", "mango");
+
+// console.log("Removed:", removed);
+// console.log("Updated array:", fruits);
+
+let fruits = ["apple", "banana", "cherry", "date", "elderberry"];
+
+let sliced = fruits.slice(1, 4);
+
+console.log("Sliced:", sliced);
+console.log("Original array:", fruits);
+
+
+
+
