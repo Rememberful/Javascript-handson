b850aa3ba1c8edd96856c6fa19a3ffcfb5f22f30
Aditya Kumar <adii.utsav@gmail.com>
2025-11-04 13:53:19 +0530

    added next day

---
+++ Day11-20/Day15/script.js
@@ -0,0 +1,11 @@
+let para1 = document.getElementById("para1");
+let para2 = document.getElementById("para2");
+
+// 1️⃣ Parent
+console.log("Parent of para1:", para1.parentElement);
+
+// 2️⃣ Next sibling of para1
+console.log("Next sibling of para1:", para1.nextElementSibling);
+
+// 3️⃣ Previous sibling of para2
+console.log("Previous sibling of para2:", para2.previousElementSibling);
