2fe04452cde46f834af0b97152594edc7e083ea0
Aditya Kumar <adii.utsav@gmail.com>
2025-12-04 18:23:14 +0530

    Studied the finally clause in the try...catch

---
+++ Day31-40/Day34/main.js
@@ -0,0 +1,11 @@
+try {
+    let a = 0;
+    console.log(program); //will create an error
+    console.log("Program executed successfully.");
+} catch (err) {
+    console.error("This is an error");  
+    console.log(p); 
+}
+finally{
+    console.log("Program continues...");
+}
