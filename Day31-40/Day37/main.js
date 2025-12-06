c337592497725747c74cf47e4cab5beae7af701a
Aditya Kumar <adii.utsav@gmail.com>
2025-12-06 09:47:54 +0530

    Added topics of localstorage

---
+++ Day31-40/Day37/main.js
@@ -0,0 +1,14 @@
+// Save
+localStorage.setItem("color", "blue");
+
+// Read
+console.log(localStorage.getItem("color"));
+
+// Update
+localStorage.setItem("color", "red");
+
+// Delete
+localStorage.removeItem("color");
+
+// Clear all
+localStorage.clear();
